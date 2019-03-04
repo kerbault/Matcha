cfb601cc0ff436f61465ab0b8261f05e854b28c4
Kevin Erbault <kerbault@z2r7p1.le-101.fr>
2019-03-04 19:04:25 +0100

    registration check duplicate + cleaner

--- controller/register.js
+++ controller/register.js
@@ -1,61 +1,73 @@
-// // check var existence
-if (req.body.login && req.body.firstName && req.body.lastName &&
-    req.body.pass && req.body.confirmpass && req.body.mail) {
+const register = (userName, firstName, lastName, password, confirmedPass, mail) => {
+    return new Promise((resolve, reject) => {
+        // // check var existence
+        if (typeof userName === "string" && typeof firstName === "string" && typeof lastName === "string" &&
+            typeof password === "string" && typeof confirmedPass === "string" && typeof mail === "string") {
 
-    // check password matching
-    if (req.body.pass === req.body.confirmpass) {
+            // check password matching
+            if (req.body.pass === req.body.confirmpass) {
 
-        // var security
-        let login = eschtml(req.body.login),
-            firstName = eschtml(req.body.firstName),
-            lastName = eschtml(req.body.lastName),
-            pass = eschtml(req.body.pass),
-            mail = eschtml(req.body.mail);
+                // var security
+                let userName = eschtml(userName),
+                    firstName = eschtml(firstName),
+                    lastName = eschtml(lastName),
+                    pass = eschtml(password),
+                    mail = eschtml(mail);
 
-        // regex definition
-        const loginRgx = /^[a-z0-9]{2,50}$/i;
-        // Minuscule, maj et nombres acceptes, taille de 4 a 25 char
+                // regex definition
+                const userNameRgx = /^[a-z0-9]{2,50}$/i;
+                // Minuscule, maj et nombres acceptes, taille de 4 a 25 char
 
-        const nameRgx = /^[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]+\.?(([',. -][a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]\.?)?[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]*\.?)*[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]?\.?$/i;
-        // Minuscule, maj, nombres et caracteres avec accents acceptes, taille de 2 a 40 char
+                const nameRgx = /^[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]+\.?(([',. -][a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]\.?)?[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]*\.?)*[a-zàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšž∂ð]?\.?$/i;
+                // Minuscule, maj, nombres et caracteres avec accents acceptes, taille de 2 a 40 char
 
-        const passRgx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,50}$/;
-        // between 8 and 50 characters, at least one uppercase letter, one lowercase letter, one number
-        // and one special character
+                const passRgx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,50}$/;
+                // between 8 and 50 characters, at least one uppercase letter, one lowercase letter, one number
+                // and one special character
 
-        const mailRgx = /^[a-z0-9._%+-]{1,64}@[a-z0-9.-]+\.[a-z]{2,255}$/i;
-        // Minuscule, maj et nombres acceptes, taille de 4 a 25 char
-
-
-        // testing regex
-        if (loginRgx.test(login) === true) {
-            if (firstName.test(nameRgx) === true) {
-                if (lastName.test(nameRgx) === true) {
-                    if (pass.test(passRgx) === true) {
-                        if (mail.test(mailRgx) === true) {
-                            connection.connect();
-                            connection.query('CREATE DATABASE IF NOT EXISTS `Matcha`', null, function (err, res) {
-                                if (err) throw err;
+                const mailRgx = /^[a-z0-9._%+-]{1,64}@[a-z0-9.-]+\.[a-z]{2,255}$/i;
+                // Minuscule, maj et nombres acceptes, taille de 4 a 25 char
 
 
-                            });
+                // testing regex
+                if (userNameRgx.test(userName) === true) {
+                    if (firstName.test(nameRgx) === true) {
+                        if (lastName.test(nameRgx) === true) {
+                            if (pass.test(passRgx) === true) {
+                                if (mail.test(mailRgx) === true) {
+                                    connection.connect((err) => {
+                                        if (err) throw err;
+                                        connection.query("SELECT COUNT(*) AS `userNameCount` FROM `users` WHERE `userName` = ?", [userName], (err, result) => {
+                                            if (err) throw err;
+                                            if (result[0].userNameCount != 0) reject(Error('userName used'));
 
+                                        });
+                                        connection.query("SELECT COUNT(*) AS `emailCount` FROM `users` WHERE `email` = ?", [email], (err, result) => {
+                                            if (err) throw err;
+                                            if (result[0].emailCount != 0) reject(Error('userName used'));
+                                        });
+                                        resolve(true);
+                                    }
+                                else
+                                    {
+                                        reject(Error('invalid mail'));
+                                    }
+                                } else {
+                                    reject(Error('invalid password'));
+                                }
+                            } else {
+                                reject(Error('invalid lastName'));
+                            }
                         } else {
-                            res.status(400).send(error, {error: 'invalid mail'})
+                            reject(Error('invalid firstName'));
                         }
                     } else {
-                        res.status(400).send(error, {error: 'invalid password'})
+                        reject(Error('invalid mail'));
                     }
-                } else {
-                    res.status(400).send(error, {error: 'invalid lastName'})
                 }
             } else {
-                res.status(400).send(error, {error: 'invalid firstName'})
+                reject(Error('missing var or invalid type'));
             }
-        } else {
-            res.status(400).send(error, {error: 'invalid mail'})
         }
-    }
-} else {
-    res.status(400).send(error, {error: 'missing var'})
-}
+    );
+};
